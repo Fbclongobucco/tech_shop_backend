cd68ad05bd0ae0fa4e8ac6f08e02f42e6e446477
longobuccodev <longobuccofbc@gmail.com>
2025-05-11 16:01:40 -0300

    Configuring the login response on authentication.

--- src/main/java/com/buccodev/tech_shop/config/CustomLoginSuccessHandler.java
+++ src/main/java/com/buccodev/tech_shop/config/CustomLoginSuccessHandler.java
@@ -3,6 +3,7 @@ package com.buccodev.tech_shop.config;
 import com.buccodev.tech_shop.exceptions.ResourceNotFoundException;
 import com.buccodev.tech_shop.repository.CustomerRepository;
 import com.buccodev.tech_shop.repository.UserSystemRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,39 +14,42 @@ import org.springframework.security.web.authentication.AuthenticationSuccessHand
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class CustomLoginSuccessHandler implements AuthenticationSuccessHandler {
 
     private final CustomerRepository customerRepository;
     private final UserSystemRepository userSystemRepository;
+    private final ObjectMapper objectMapper;
 
 
-    public CustomLoginSuccessHandler(CustomerRepository customerRepository, UserSystemRepository userSystemRepository) {
+    public CustomLoginSuccessHandler(CustomerRepository customerRepository, UserSystemRepository userSystemRepository, ObjectMapper objectMapper) {
         this.customerRepository = customerRepository;
         this.userSystemRepository = userSystemRepository;
+        this.objectMapper = objectMapper;
     }
 
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+
+        response.setContentType("application/json");
+
         String username = authentication.getName();
 
-        if (customerRepository.findByEmail(username).isPresent()) {
-            response.setContentType("application/json");
-            response.getWriter().write(
-                    String.format("{\"message\": \"User %s logged in\"}", username)
-            );
+        boolean isCustomer = customerRepository.findByEmail(username).isPresent();
+        boolean isSystemUser = userSystemRepository.findByEmail(username).isPresent();
+
+        if (isCustomer || isSystemUser) {
+            Map<String, String> jsonResponse = new HashMap<>();
+            jsonResponse.put("emailLogged", username);
+
+            response.getWriter().write(objectMapper.writeValueAsString(jsonResponse));
             return;
         }
 
-        if (userSystemRepository.findByEmail(username).isPresent()) {
-            response.setContentType("application/json");
-            response.getWriter().write(
-                    String.format("{\"message\": \"System user %s logged in\"}", username)
-            );
-            return;
-        }
         response.sendRedirect("/login?error");
 
     }
