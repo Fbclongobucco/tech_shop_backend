0e77082c81cf5c77c19986d4ac63e1c5868f455a
longobuccodev <longobuccofbc@gmail.com>
2025-06-04 18:58:32 -0300

    Created the product photo upload service

---
+++ src/main/java/com/buccodev/tech_shop/config/MinioConfig.java
@@ -0,0 +1,18 @@
+package com.buccodev.tech_shop.config;
+
+import io.minio.MinioClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MinioConfig {
+
+    @Bean
+    public MinioClient minioClient() {
+        return MinioClient.builder()
+                .endpoint("http://localhost:9000")
+                .credentials("longobucco", "F@b917109")
+                .build();
+    }
+
+}
---
+++ src/main/java/com/buccodev/tech_shop/services/MinioService.java
@@ -0,0 +1,42 @@
+package com.buccodev.tech_shop.services;
+
+import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.UUID;
+
+@Service
+public class MinioService {
+
+    private final MinioClient minioClient;
+
+    public MinioService(MinioClient minioClient) {
+        this.minioClient = minioClient;
+    }
+
+    public String uploadPhoto(MultipartFile photo) throws IOException {
+
+        var fileName = UUID.randomUUID().toString();
+        try {
+            var file = photo.getInputStream();
+            minioClient.putObject(
+                   PutObjectArgs.builder()
+                           .bucket("tech-shop-products")
+                           .object(fileName)
+                           .stream(file, photo.getSize(), -1)
+                           .contentType("image/png")
+                           .build()
+
+            );
+            return "http://localhost:9000/tech-shop-products/" + fileName;
+        } catch (Exception e) {
+            throw new FileNotFoundException("error while uploading file");
+        }
+
+    }
+
+}
